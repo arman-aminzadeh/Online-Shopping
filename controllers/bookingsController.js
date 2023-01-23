b9b1793de139a8f4ff21ccb2c6e56a38020a5865
arman aminzadeh <arman.aminzadeh@gmail.com>
2023-01-23 23:33:56 +0100

    Working on Stripe

--- controllers/bookingsController.js
+++ controllers/bookingsController.js
@@ -13,22 +13,41 @@ exports.getCheckOutSession = async (req, res, next) => {
     // create check out session
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
-      success_url: `${req.protocol}://${req.get("host")}/`,
-      cancel_url: `${req.protocol}://${req.get("host")}/tours/${product.id}`,
       customer_email: req.user.email,
       client_reference_id: req.params.productId,
       line_items: [
         {
-          name: `${product.title}`,
-          description: product.description,
-          //images: product.imageCover,
-          //amount: product.price * 100,
-          amount: 49.5 * 100,
-          currency: "usd",
+          price_data: {
+            currency: "usd",
+            unit_amount: (49, 99 * 100),
+            product_data: {
+              name: `${product.title}`,
+              description: `${product.description}`,
+            },
+          },
           quantity: 1,
         },
       ],
+      mode: "payment",
+      success_url: `${req.protocol}://${req.get("host")}/`,
+      cancel_url: `${req.protocol}://${req.get("host")}/tours/${product.id}`,
     });
+    // const session = await stripe.checkout.sessions.create({
+    //   payment_method_types: ["card"],
+    //   success_url: `${req.protocol}://${req.get("host")}/`,
+    //   cancel_url: `${req.protocol}://${req.get("host")}/tours/${product.id}`,
+    //   customer_email: req.user.email,
+    //   client_reference_id: req.params.productId,
+    //   line_items: [
+    //     {
+    //       name: `${product.title}`,
+    //       //images: product.imageCover,
+    //       // price_data: 49 * 100,
+    //       currency: "usd",
+    //       quantity: 1,
+    //     },
+    //   ],
+    // });
     // create session as response
     res.status(200).json({
       status: "success",
